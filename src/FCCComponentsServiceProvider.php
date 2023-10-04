bae8537ba9ae11475c63142b628ad7e8f3003e7d
Tim Smith <tim.smith@titan21.co.uk>
2023-10-04 19:47:39 +0100

    Adding components

--- src/FCCComponentsServiceProvider.php
+++ src/FCCComponentsServiceProvider.php
@@ -4,6 +4,7 @@ namespace Titantwentyone\FCCComponents;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Titantwentyone\FCCComponents\Components\Columns;
 
 class FCCComponentsServiceProvider extends PackageServiceProvider
 {
@@ -11,5 +12,12 @@ class FCCComponentsServiceProvider extends PackageServiceProvider
     {
         $package->name('fcc-components')
             ->hasViews('fcc');
+    }
+
+    public function bootingPackage()
+    {
+        \Titantwentyone\FilamentContentComponents\ComponentRegister::registerComponents([
+            Columns::class
+        ]);
     }
 }
